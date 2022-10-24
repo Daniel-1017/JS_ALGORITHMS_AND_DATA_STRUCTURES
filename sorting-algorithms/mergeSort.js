962335ba075aef59ac2f55e66c50c7ab2b003500
Daniel-17 <frimudaniel17@gmail.com>
2022-10-24 22:07:07 +0200

    merge sort

---
+++ sorting-algorithms/mergeSort.js
@@ -0,0 +1,42 @@
+const merge = (arr1, arr2) => {
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+};
+
+const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+};
+
+mergeSort([50, 24, 66, 10, 1, 55]);
+
+/* 
+TIME COMPLEXITY
+best - O(n log n)
+average - O(n log n)
+worst - O(n log n)
+
+SPACE COMPLEXITY - O(n)
+*/
