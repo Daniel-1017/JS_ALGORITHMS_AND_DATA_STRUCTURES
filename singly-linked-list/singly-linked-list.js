efb7edaa50165dc211fbc9a651a5b631c98e9a5f
Daniel-17 <frimudaniel17@gmail.com>
2022-10-25 18:42:59 +0200

    Node class + SinglyLinkedList class + push method

---
+++ singly-linked-list/singly-linked-list.js
@@ -0,0 +1,34 @@
+// A LINKED LIST IS JUST A COLLECTION OF NODES
+
+// NODE
+// STORES A PIECE OF DATA - VAL
+// REFERENCE TO NEXT NODE - NEXT
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const list = new SinglyLinkedList();
