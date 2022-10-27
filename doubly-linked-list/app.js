8a0c371fbc3a26bcb9498d2e9b09bc4747074189
Daniel-17 <frimudaniel17@gmail.com>
2022-10-27 16:44:12 +0200

    pop method

--- doubly-linked-list/app.js
+++ doubly-linked-list/app.js
@@ -25,6 +25,20 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if (!this.head) return undefined;
+    const currentTail = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = currentTail.prev;
+      this.tail.next = null;
+      currentTail.prev = null;
+    }
+    this.length--;
+    return currentTail;
+  }
 }
 
 const first = new DoublyLinkedList();
