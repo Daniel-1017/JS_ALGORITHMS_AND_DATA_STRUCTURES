dfb96811351d119469ab498bc4e18f663d3c63c3
Daniel-17 <frimudaniel17@gmail.com>
2022-10-27 16:30:37 +0200

    push method

--- doubly-linked-list/app.js
+++ doubly-linked-list/app.js
@@ -12,4 +12,19 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    const node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+const first = new DoublyLinkedList();
