7fa212ca72f3bb296df5c90fd866a306877506df
Daniel-17 <frimudaniel17@gmail.com>
2022-10-29 15:22:48 +0200

    queues

---
+++ stacks-and-queues/queues.js
@@ -0,0 +1,46 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(val) {
+    const node = new Node(val);
+    if (!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      this.last.next = node;
+      this.last = node;
+    }
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+    const temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.val;
+  }
+}
+
+const queue = new Queue();
+
+/* 
+BIG O OF STACK
+insertion - O(1)
+removal - O(1)
+searching - O(n)
+access  - O(n)
+*/
