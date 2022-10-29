7cfb638fedcf8a964115fcf7e38dfb6b8b8e0a90
Daniel-17 <frimudaniel17@gmail.com>
2022-10-29 16:51:36 +0200

    BST insert method

--- trees/binary-search-tree.js
+++ trees/binary-search-tree.js
@@ -10,4 +10,30 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(val) {
+    const node = new Node(val);
+    if (!this.root) {
+      this.root = node;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (val === current.val) return undefined;
+      if (val < current.val) {
+        if (current.left === null) {
+          current.left = node;
+          return this;
+        }
+        current = current.left;
+      } else if (val > current.val) {
+        if (current.right === null) {
+          current.right = node;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
 }
+
+const BST = new BinarySearchTree();
