6693e155ae4256ab8de4762c941c58051c6251e1
Daniel-17 <frimudaniel17@gmail.com>
2022-10-31 11:30:30 +0100

    tree traversal - DFSInOrder

---
+++ tree-traversal/DFSInOrder.js
@@ -0,0 +1,28 @@
+// DEAPTH FOR SEARCH - InOrder
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+  DFSInOrder() {
+    let data = [];
+    let current = this.root;
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.val);
+      if (node.right) traverse(node.right);
+    }
+    traverse(current);
+
+    return data;
+  }
+}
+
+const tree = new BinaryTree();
