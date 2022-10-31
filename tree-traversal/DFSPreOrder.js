77649f0d561931e42c3c29c2332a6eb5014f449c
Daniel-17 <frimudaniel17@gmail.com>
2022-10-31 11:18:12 +0100

    tree traversal - DFSPreOrder

---
+++ tree-traversal/DFSPreOrder.js
@@ -0,0 +1,28 @@
+// DEAPTH FOR SEARCH - PreOrder
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+  DFSPreOrder() {
+    let data = [];
+    let current = this.root;
+    function traverse(node) {
+      data.push(node.val);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(current);
+
+    return data;
+  }
+}
+
+const tree = new BinaryTree();
