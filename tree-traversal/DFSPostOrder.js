8d14d8b05aed1864475f4050db45a842f7c42275
Daniel-17 <frimudaniel17@gmail.com>
2022-10-31 11:25:31 +0100

    tree traversal - DFSPostOrder

---
+++ tree-traversal/DFSPostOrder.js
@@ -0,0 +1,28 @@
+// DEAPTH FOR SEARCH - PostOrder
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+  DFSPostOrder() {
+    let data = [];
+    let current = this.root;
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.val);
+    }
+    traverse(current);
+
+    return data;
+  }
+}
+
+const tree = new BinaryTree();
