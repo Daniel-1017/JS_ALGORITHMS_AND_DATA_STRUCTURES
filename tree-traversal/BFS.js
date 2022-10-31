fa411f0420126ae9ca9120f1957da6879f90c158
Daniel-17 <frimudaniel17@gmail.com>
2022-10-31 11:03:26 +0100

    tree traversal - BFS

---
+++ tree-traversal/BFS.js
@@ -0,0 +1,29 @@
+// BREATH FOR SEARCH
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+  BFS() {
+    let data = [],
+      queue = [],
+      node = this.root;
+    queue.push(this.root);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+}
+
+const tree = new BinaryTree();
