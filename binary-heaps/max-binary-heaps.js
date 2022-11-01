187b5d1cf943c7eac874993fe4f7243bfeee72b1
Daniel-17 <frimudaniel17@gmail.com>
2022-11-01 13:44:43 +0100

    MBH - extractMax method

--- binary-heaps/max-binary-heaps.js
+++ binary-heaps/max-binary-heaps.js
@@ -1,6 +1,6 @@
 class MaxBinaryHeap {
   constructor() {
-    this.val = [41, 39, 33, 18, 27, 12];
+    this.val = [55, 39, 41, 18, 27, 12, 33];
   }
   insert(val) {
     this.val.push(val);
@@ -16,6 +16,47 @@ class MaxBinaryHeap {
       this.val[parentIdx] = element;
       this.val[idx] = parent;
       idx = parentIdx;
+    }
+  }
+  extractMax() {
+    const max = this.val[0];
+    const end = this.val.pop();
+    if (this.val.length > 0) {
+      this.val[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.val.length;
+    const element = this.val[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.val[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.val[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) break;
+      this.val[idx] = this.val[swap];
+      this.val[swap] = element;
+      idx = swap;
     }
   }
 }
