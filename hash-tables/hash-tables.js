d15c30699433d1b69afd7fb3a517dcec955d5707
Daniel-17 <frimudaniel17@gmail.com>
2022-11-02 13:02:33 +0100

    hash tables class + get and set method

--- hash-tables/hash-tables.js
+++ hash-tables/hash-tables.js
@@ -19,4 +19,41 @@ const hash2 = (key, arrayLen) => {
   return total;
 };
 
-console.log(hash2("cyan", 13));
+class HashTable {
+  constructor(size = 5) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  set(key, val) {
+    const idx = this._hash(key);
+    if (!this.keyMap[idx]) {
+      this.keyMap[idx] = [];
+    }
+    this.keyMap[idx].push([key, val]);
+  }
+
+  get(key) {
+    const idx = this._hash(key);
+    if (this.keyMap[idx]) {
+      for (let item of this.keyMap[idx]) {
+        if (item[0] === key) {
+          return item[1];
+        }
+      }
+    }
+    return undefined;
+  }
+}
+
+const hashTable = new HashTable();
