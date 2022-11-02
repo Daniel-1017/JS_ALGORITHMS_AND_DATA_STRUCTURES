8b695e38f1ec4539477243f96ffe5d5841b9f608
Daniel-17 <frimudaniel17@gmail.com>
2022-11-02 12:25:19 +0100

    hash tables - increasing performance

--- hash-tables/hash-tables.js
+++ hash-tables/hash-tables.js
@@ -7,3 +7,16 @@ const hash = (key, arrayLen) => {
   }
   return total;
 };
+
+const hash2 = (key, arrayLen) => {
+  let total = 0;
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96;
+    total = (total * WEIRD_PRIME + value) % arrayLen;
+  }
+  return total;
+};
+
+console.log(hash2("cyan", 13));
