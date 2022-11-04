d758e5334274339bf5d124baa3f7b89726281e9d
Daniel-17 <frimudaniel17@gmail.com>
2022-11-04 16:00:10 +0100

    addEdge method

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -5,6 +5,11 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+  addEdge(vertex1, vertex2) {
+    const vertex1Key = this.adjacencyList[vertex1];
+    const vertex2Key = this.adjacencyList[vertex2];
+    vertex1Key.push(vertex2), vertex2Key.push(vertex1);
+  }
 }
 
 const graph = new Graph();
