969d25cad7ddbb8254b01b6b0067430b45b00852
Daniel-17 <frimudaniel17@gmail.com>
2022-11-07 18:58:22 +0100

    dijkstra's algorithm

--- dijkstra-algorithm/app.js
+++ dijkstra-algorithm/app.js
@@ -1,19 +1,3 @@
-class PriorityQueue {
-  constructor() {
-    this.val = [];
-  }
-  enqueue(val, priority) {
-    this.val.push({ val, priority });
-    this.sort();
-  }
-  dequeue() {
-    return this.val.shift();
-  }
-  sort() {
-    this.val.sort((a, b) => a.priority - b.priority);
-  }
-}
-
 class WeightedGraph {
   constructor() {
     this.adjacencyList = {};
@@ -24,6 +8,131 @@ class WeightedGraph {
   addEdge(vertex1, vertex2, weight) {
     this.adjacencyList[vertex1].push({ node: vertex2, weight });
     this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
+  dijkstra(start, finish) {
+    const nodes = new PriorityQueue();
+    const distances = {};
+    const previeous = {};
+    let path = []; // to return at end
+    let smallest;
+
+    // build up initial state
+    for (let vertex in this.adjacencyList) {
+      if (vertex === start) {
+        distances[vertex] = 0;
+        nodes.enqueue(vertex, 0);
+      } else {
+        distances[vertex] = Infinity;
+        nodes.enqueue(vertex, Infinity);
+      }
+      previeous[vertex] = null;
+    }
+
+    // as long as there is something to visit
+    while (nodes.val.length) {
+      smallest = nodes.dequeue().val;
+      if (smallest === finish) {
+        // WE ARE DONE
+        // BUILD PATH TO RETURN
+        while (previeous[smallest]) {
+          path.push(smallest);
+          smallest = previeous[smallest];
+        }
+        break;
+      }
+      if (smallest || distances[smallest] !== Infinity) {
+        for (let neighbor in this.adjacencyList[smallest]) {
+          // find neighboring node
+          let nextNode = this.adjacencyList[smallest][neighbor];
+
+          // calculate new distance to neighboring node
+          let candidate = distances[smallest] + nextNode.weight;
+          let nextNeighbor = nextNode.node;
+          if (candidate < distances[nextNeighbor]) {
+            // updating new smallest distance to neighbor
+            distances[nextNeighbor] = candidate;
+
+            // updating previeous - How we got to neighbor
+            previeous[nextNeighbor] = smallest;
+
+            // enqueue in priority queue with new priority
+            nodes.enqueue(nextNeighbor, candidate);
+          }
+        }
+      }
+    }
+    return path.concat(smallest).reverse();
+  }
+}
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.val = [];
+  }
+  enqueue(val, priority) {
+    const node = new Node(val, priority);
+    this.val.push(node);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.val.length - 1;
+    const element = this.val[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.val[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.val[parentIdx] = element;
+      this.val[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  dequeue() {
+    const min = this.val[0];
+    const end = this.val.pop();
+    if (this.val.length > 0) {
+      this.val[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.val.length;
+    const element = this.val[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.val[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.val[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) break;
+      this.val[idx] = this.val[swap];
+      this.val[swap] = element;
+      idx = swap;
+    }
   }
 }
 
